d30cc9c798d8293b138d31e22ddb1f16ec372e09
DVNJNKNS <devonmjenkins3@gmail.com>
2022-01-29 15:13:13 -0500

    lab created and formatted

--- app.js
+++ app.js
@@ -3,8 +3,15 @@ const game = {
   biggestNum: 100,
   smallestNum: 1,
   secretNum: null,
+  previousGuesses:[],
   play: function() {
     this.secretNum = Math.floor(Math.random() * 
       (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
+  },
+getGuess: function(){ while (i <= this.biggestNum)  (i>=this.smallestNum); 
+    return `Enter a guess between ${this.smallestNum} and ${this.biggestNum} `
+
   }
 }
+
+console.log(game.getGuess())
