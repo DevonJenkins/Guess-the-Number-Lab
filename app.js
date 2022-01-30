e00bcee137aa1ab46ef4b30004cd3d729647aab4
DVNJNKNS <devonmjenkins3@gmail.com>
2022-01-30 12:14:07 -0500

    added pseudocode to guess count function

--- app.js
+++ app.js
@@ -4,30 +4,38 @@ const game = {
   smallestNum: 1,
   secretNum: null,
   previousGuesses:[],
+  input = 0, 
+  
+///you need to render a guess count function that takes the current guess and compares it to the mystery number. If the guess isn't a number or is outside the guess range, then keep running the get guess function. This is similar to the get guesses function, but its purpose is to either stop the loop or keep the loop running       
+
   play: function() {
     this.secretNum = Math.floor(Math.random() * 
       (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
   },
-
+//this function is similar to the one above, but the purpose is to get the guess, compare it to the mysteryNum 
 
 
   getGuess: function(){ 
   let gettingGuess = true
   let input = null 
-   //use while loop 
-   //while (playerGuess != secretNum) { add to previous guesses and contrinue the game
+  guess = parseInt() 
+  
+  return `Enter a guess between ${this.smallestNum} and ${this.biggestNum} `
+  
+  
+  //use while loop 
+   //while (input != secretNum) { add to previous guesses and contrinue the game
      //}
      //
-//if (guess == SecretNum) {return "congrats! its a match!" 
-// } else if(guess > secretNum) { return "you're too high muf'k"
-// } else if(guess < secretNum) {return "you're too low muf'k"
+//if (input == this.SecretNum) {return "congrats! its a match!" and end play loop
+// } else if(input > this.secretNum) { return "you're too high muf'k"
+// } else if(input < this.secretNum) {return "you're too low muf'k"
 // } else { return 'this answer is invalid' 
 
 
  //  }
     
-    return `Enter a guess between ${this.smallestNum} and ${this.biggestNum} `
-
+    this.render()
   }
 }
 
