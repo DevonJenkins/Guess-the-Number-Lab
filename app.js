1dc4d5d873da2ff72ff1c5514bc89f66e23871ec
DVNJNKNS <devonmjenkins3@gmail.com>
2022-01-29 21:00:45 -0500

    added some pseudocode to start figuring out the conditional

--- app.js
+++ app.js
@@ -8,10 +8,28 @@ const game = {
     this.secretNum = Math.floor(Math.random() * 
       (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
   },
-getGuess: function(){ while (i <= this.biggestNum)  (i>=this.smallestNum); 
+
+
+
+  getGuess: function(){ 
+  let gettingGuess = true
+  let input = null 
+   //use while loop 
+   //while (playerGuess != secretNum) { add to previous guesses and contrinue the game
+     //}
+     //
+//if (guess == SecretNum) {return "congrats! its a match!" 
+// } else if(guess > secretNum) { return "you're too high muf'k"
+// } else if(guess < secretNum) {return "you're too low muf'k"
+// } else { return 'this answer is invalid' 
+
+
+ //  }
+    
     return `Enter a guess between ${this.smallestNum} and ${this.biggestNum} `
 
   }
 }
+
 
 console.log(game.getGuess())
